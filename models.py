b17fcd0fae29704231a78dfcb095245358daf2c7
Дмитрий <dmitriyredozubov@gmail.com>
2020-12-03 23:03:46 +0700

    database configured

--- models.py
+++ models.py
@@ -2,18 +2,28 @@
 # Python 3.8.6
 
 
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, VARCHAR, String, LargeBinary
+from settings import engine
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 Base = declarative_base()
 
 
 class User(Base):
+    __tablename__ = 'user'
+
     id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
     telegram_id = Column(Integer, nullable=False, unique=True)
     photo = Column(LargeBinary, nullable=True)
-    name = Column(String, nullable=True)
+    name = Column(String(64), nullable=True)
     company = Column(VARCHAR, nullable=True)
     interests = Column(VARCHAR, nullable=True)
     usefulness = Column(VARCHAR, nullable=True)
 
+
+Base.metadata.create_all(engine)
--- settings.py
+++ settings.py
@@ -1,2 +1,6 @@
+from sqlalchemy import create_engine
+
+
 
 TOKEN = '1431183102:AAF95F0wUZovGBGsgg4tePQzbw69Fef0d6o'
+engine = create_engine('postgresql://postgres:root@localhost:5555/alchemy', echo=False)
